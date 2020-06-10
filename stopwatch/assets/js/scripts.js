1350a0c09e71140690c30df0fa9eebfb71a73530
Chris Drew <chris@leaplogic.net>
2020-06-10 09:57:41 -0500

    requirements and init build w/ seconds and mills

--- stopwatch/assets/js/scripts.js
+++ stopwatch/assets/js/scripts.js
@@ -1,17 +1,35 @@
 //Requirements:
-// 
-let second = setInterval(1000)
+// create stopwatch with 4 number time format 00:00
+// display seconds & milliseconds until 1 minute is reached
+// display minutes and seconds until 1 hour is reached
+// display hours and minutes until infinity
 
-function start(){
-// if time is not 0, then...
-// if time is 0 then add 1 millisecond every millisecond until 1000 miliseconds is reached
-}
+let start = document.getElementById('start')
+let end = document.getElementById('end')
+let reset = document.getElementById('reset')
 
-function stop(){
-// if time is not 0 then stop timer
-// if 
-}
+let startTime = Date.now();
+let Elapsed = document.getElementById('timeElapsed')
 
-function reset(){
+let seconds = setInterval( function(){
+    // Set difference = difference between epoch time and current time, every second
+    let difference = Date.now() - startTime;
+    let secs = Math.floor(difference / 1000);
+    // console.log(secs);
+    return secs;
+    }, 1000);
 
-}
+    
+let milliseconds = setInterval( function(){
+    let difference = Date.now() - startTime;
+    let mill = (difference / 1000);
+    let remainer = (mill % 1).toFixed(2);
+    var decPart = (remainer+"").split(".")[1];
+    }, 10);
+
+console.log(decPart);
+
+let minutes = setInterval( function(){
+    console.log("1 minute");
+    // min++
+    }, 60000);
