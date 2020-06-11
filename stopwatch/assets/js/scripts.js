99e9d1e18b5104b107f2b39155eabae6e76da520
Chris Drew <chris@leaplogic.net>
2020-06-10 21:30:50 -0500

    added dom vars and got time to print to html

--- stopwatch/assets/js/scripts.js
+++ stopwatch/assets/js/scripts.js
@@ -9,28 +9,31 @@ let end = document.getElementById('end')
 let reset = document.getElementById('reset')
 
 let startTime = Date.now();
-let elapsed = document.getElementById('timeElapsed')
+let hoursElapsed = document.getElementById('hoursElapsed')
+let minsElapsed = document.getElementById('minsElapsed')
+let secsElapsed = document.getElementById('secsElapsed')
+let millsElapsed = document.getElementById('millsElapsed')
 
 //Seconds calculation
-let seconds = setInterval( function(){
-    // Set difference = difference between epoch time and current time, every second
-    let difference = Date.now() - startTime;
-    let secs = Math.floor(difference / 1000);
-    // console.log(secs);
-    return secs;
+    let seconds = setInterval( function(){
+        // Set difference = difference between epoch time and current time, every second
+        let difference = Date.now() - startTime;
+        let secs = Math.floor(difference / 1000);
+        // console.log(secs);
+        secsElapsed.innerHTML = secs;
     }, 1000);
 
     //milliseconds calculation
-    setInterval( function millReturn(){
-    let difference = Date.now() - startTime;
-    let mill = (difference / 1000);
-    let remainer = (mill % 1).toFixed(2);
-    decPart = (remainer+"").split(".")[1];
-    // console.log(decPart)
-    return decPart
+    let mills = setInterval( function(){
+        let difference = Date.now() - startTime;
+        let mill = (difference / 1000);
+        let remainer = (mill % 1).toFixed(2);
+        decPart = (remainer+"").split(".")[1];
+        // console.log(decPart)
+        millsElapsed.innerHTML = decPart;
     }, 10);
 
-let minutes = setInterval( function(){
-    console.log("1 minute");
-    // min++
+    let mins = setInterval( function(){
+        console.log("1 minute");
+        // min++
     }, 60000);
