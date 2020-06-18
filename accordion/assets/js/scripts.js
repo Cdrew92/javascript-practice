91074ec0e7273cc62967d6c5b370c9b17e887a1a
Chris Drew <chris@leaplogic.net>
2020-06-18 16:15:02 -0500

    assigned id's to text bodies and buttons

--- accordion/assets/js/scripts.js
+++ accordion/assets/js/scripts.js
@@ -1,14 +1,19 @@
 //Requirements:
 //Create an accordion using 3 buttons and blocks of text
-//ID's should not be used. Refer to DOM elements using only NodeList positions
+//ID's should not be created in index.html. create ID's in script to then target elements
 
-document.addEventListener('DOMContentLoaded', (event) => {
 
-    let collapseButtons = document.querySelectorAll('.collapseButton')
-    let collapseBodies = document.querySelectorAll('.collapseBody') 
+    let collapseButtons = document.querySelectorAll('.collapseButton');
+    let collapseBodies = document.querySelectorAll('.collapseBody');
+    let btnArray = [].slice.call(collapseButtons);
 
-    for(i=0;i<=collapseBodies.length; i++){
+    //set display = none on page load & assign ID's to bodies
+    for(i=0;i<collapseBodies.length; i++){
         collapseBodies[i].style.display = 'none';
+        collapseBodies[i].setAttribute('id', "body" + [i + 1])
     }
 
-})
+    //assign ID's to buttons
+    for(const [i , item] of btnArray.entries()){
+        item.setAttribute('id', "button" + [i + 1]);
+    }
