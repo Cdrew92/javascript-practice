c2aa5cc44b6849e3f534d8dfd0218aea08120ef9
Chris Drew <chris@leaplogic.net>
2020-06-19 13:17:22 -0500

    added collapse and expand functionality

--- accordion/assets/js/scripts.js
+++ accordion/assets/js/scripts.js
@@ -3,17 +3,25 @@
 //ID's should not be created in index.html. create ID's in script to then target elements
 
 
-    let collapseButtons = document.querySelectorAll('.collapseButton');
-    let collapseBodies = document.querySelectorAll('.collapseBody');
-    let btnArray = [].slice.call(collapseButtons);
+let collapseButtons = document.querySelectorAll('.collapseButton');
+let collapseBodies = document.querySelectorAll('.collapseBody');
+let btnArray = [].slice.call(collapseButtons); //coerce Nodelist into array of buttons
 
-    //set display = none on page load & assign ID's to bodies
-    for(i=0;i<collapseBodies.length; i++){
-        collapseBodies[i].style.display = 'none';
-        collapseBodies[i].setAttribute('id', "body" + [i + 1])
-    }
+for(i=0;i<collapseBodies.length; i++){
+    collapseBodies[i].style.display = 'none';
+}
 
-    //assign ID's to buttons
-    for(const [i , item] of btnArray.entries()){
-        item.setAttribute('id', "button" + [i + 1]);
-    }
+//assign ID's to buttons & Bodies
+for(const [i , item] of btnArray.entries()){
+    item.setAttribute('id', "button" + [i + 1]);
+    let button = document.getElementById('button' + [i + 1]);
+    // set onclick event listener to expand/collapse elements
+    button.onclick = function expandCollapse() {
+        let count = 0;
+        if(collapseBodies[i].style.display === 'none') {
+            collapseBodies[i].style.display = 'block';
+        } else {
+            collapseBodies[i].style.display = 'none';
+        }
+    };
+}
