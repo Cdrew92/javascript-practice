916ee5672eacffa9bcaa5ee0076286bd7839dd48
Chris Drew <chris@leaplogic.net>
2020-06-24 10:36:00 -0500

    added requirements and secondary goal

--- scroll-progress-bar/assets/js/scripts.js
+++ scroll-progress-bar/assets/js/scripts.js
@@ -1,3 +1,5 @@
+//requirements: Create scrollbar that moves horizontailly across the screen and reaches 100% width once the user has scrolled to the bottm of the page content
+//Goal: calculate estimated read time based on 130wpm by counting number of words in content div and printing to DOM
 const scrollBar = document.getElementById('scrollBar');
 const content = document.querySelector(".content");
 
@@ -19,7 +21,7 @@ window.onscroll = function getYOffset() {
     yOffset = window.pageYOffset;
     contentOffset = height - yOffset;
     scrollBar.style.minWidth = "1%";
-    progressCalc = (yOffset / ((height + 16)- screenHeight)) * 100;
+    progressCalc = (yOffset / ((height + 16) - screenHeight)) * 100;
     if (progressCalc <= 100) {
         scrollBar.style.width = progressCalc +"%"
     } else {
