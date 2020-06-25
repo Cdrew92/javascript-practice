32fb48155625a73bcbfe3e63890e8be9614a59f9
Chris Drew <chris@leaplogic.net>
2020-06-25 16:36:15 -0500

    Made readingTime calcs part of onscroll func

--- scroll-progress-bar/assets/js/scripts.js
+++ scroll-progress-bar/assets/js/scripts.js
@@ -8,11 +8,7 @@ let screenHeight = window.screen.height;
 let wordsInContent = content.innerText;
 let wordCount = wordsInContent.match(/(\w+)/g).length; //Regex - \w+ (matches all characters in a word and pushes to wordCount array) - g (greedy. continues applying \w+ until all words are pushed to wordCount array)
 let minutesOfReading = parseInt(wordCount / 130);
-if (minutesOfReading <= 1) {
-readTimeLabel.innerHTML += minutesOfReading + ' Minute' ;
-} else {
-    readTimeLabel.innerHTML += minutesOfReading + ' Minutes';
-}
+readTimeLabel.innerHTML = "Read Time: " + minutesOfReading + " Minutes"; //sets value on page load
 
 window.onresize = function(){
     contentHeight = content.offsetHeight;
@@ -21,12 +17,17 @@ window.onresize = function(){
 
 window.onscroll = function getYOffset() {
     let scrollOffsetPx = window.pageYOffset;
-    let progressCalc = (scrollOffsetPx / (contentHeight - screenHeight)) * 100;
+    let screenWidthSetting = (scrollOffsetPx / (contentHeight - screenHeight)) * 100; //Calculates (Pixels from top edge of screen / (Height of content - total height of viewport) Ex. 40% = (200 / (1000 - 500)) 
+    scrollBar.style.minWidth = "1%"; // Sets default value
 
-    scrollBar.style.minWidth = "1%";
+    let minutesLeft = Math.round(minutesOfReading * (screenWidthSetting / 100));     //Calculates minutes of reading remaining by piggybacking off screen width percentage(screenWidthSetting) EX: (19 * (100 / 100) = 19
+    minutesLeft = parseInt(minutesOfReading - minutesLeft); // Gets inverse of minutesLeft Ex: 19 - 14 = 5 minutes left - so counter counts down instead of up
+    console.log(minutesLeft);
+    readTimeLabel.innerHTML = "Read Time: " + minutesLeft + " Minutes" ;
     
-    if (progressCalc <= 100) {
-        scrollBar.style.width = progressCalc +"%"
+    if (screenWidthSetting <= 100) {
+        scrollBar.style.width = screenWidthSetting + "%";
+        readTimeLabel.style.top = screenWidthSetting + "%";
     } else {
         scrollBar.style.width = "100%"
     }
