5d8ca8b9d03cced4f25678bba42f22f3d912ef24
Chris Drew <chris@leaplogic.net>
2020-06-30 13:11:43 -0500

    removed read time scroll logic & tuned math.round

--- scroll-progress-bar/assets/js/scripts.js
+++ scroll-progress-bar/assets/js/scripts.js
@@ -17,17 +17,15 @@ window.onresize = function(){
 
 window.onscroll = function getYOffset() {
     let scrollOffsetPx = window.pageYOffset;
-    let screenWidthSetting = (scrollOffsetPx / (contentHeight - screenHeight)) * 100; //Calculates (Pixels from top edge of screen / (Height of content - total height of viewport) Ex. 40% = (200 / (1000 - 500)) 
+    let screenWidthSetting = Math.round(scrollOffsetPx / (contentHeight - screenHeight) * 100); //Calculates (Pixels from top edge of screen / (Height of content - total height of viewport) Ex. 40% = (200 / (1000 - 500)) 
     scrollBar.style.minWidth = "1%"; // Sets default value
 
-    let minutesLeft = Math.round(minutesOfReading * (screenWidthSetting / 100));     //Calculates minutes of reading remaining by piggybacking off screen width percentage(screenWidthSetting) EX: (19 * (100 / 100) = 19
+    let minutesLeft = minutesOfReading * (screenWidthSetting / 100);     //Calculates minutes of reading remaining by piggybacking off screen width percentage(screenWidthSetting) EX: (19 * (100 / 100) = 19
     minutesLeft = parseInt(minutesOfReading - minutesLeft); // Gets inverse of minutesLeft Ex: 19 - 14 = 5 minutes left - so counter counts down instead of up
-    console.log(minutesLeft);
     readTimeLabel.innerHTML = "Read Time: " + minutesLeft + " Minutes" ;
     
     if (screenWidthSetting <= 100) {
         scrollBar.style.width = screenWidthSetting + "%";
-        readTimeLabel.style.top = screenWidthSetting + "%";
     } else {
         scrollBar.style.width = "100%"
     }
