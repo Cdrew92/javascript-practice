64ed85cdbc6cfafab3e62597c73c153ba78fdcdd
Chris Drew <chris@leaplogic.net>
2020-07-07 17:10:17 -0500

    allows 2 decimals only on disallowed char input

--- pricing-calculator/assets/js/scripts.js
+++ pricing-calculator/assets/js/scripts.js
@@ -29,11 +29,11 @@ function setInputFilter(textbox, inputFilter) {
     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
       textbox.addEventListener(event, function() {
         if (inputFilter(this.value)) {
-          this.oldValue = this.value;
+          this.oldValue = parseFloat(this.value).toFixed(2);
           this.oldSelectionStart = this.selectionStart; //prevents cursor from moving back in front of string
           this.oldSelectionEnd = this.selectionEnd;
         } else if (this.hasOwnProperty("oldValue")) { // if the if statement above evaluates false, which means the characters being input are not numeric as defined in setInputFilter(), else if checks to see if textbox has oldValue as a property and replaces current value with oldValue
-            this.value = this.oldValue;
+            this.value = parseFloat(this.oldValue).toFixed(2);
             this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
         } else { //catch all just in case unexpected character is input
             this.value = "";
