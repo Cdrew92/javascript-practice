81bb13af2edfd78effa52d70c99d56e1317687ed
Chris Drew <chris@leaplogic.net>
2020-07-22 10:10:30 -0500

    refactored code into easier to read format

--- calculator/assets/js/scripts.js
+++ calculator/assets/js/scripts.js
@@ -1,83 +1,76 @@
 let allButtons = document.querySelectorAll("button");
 let input = document.getElementById("input");
-let oldNum = "";
+let num1 = "";
 let operator = "";
-let currentNum = "";
+let num2 = "";
+let lengthCheck = "";
 let result = "";
 
 let value = allButtons.forEach(element => {
     element.onclick = function changeContent() { 
-        if (oldNum == "" && currentNum == "") { //evaluates if oldNum and currentNum are undefined
-            operator = "";
+        //allow for negative number and num1 input only if num1 has no value AND no operator has been selected
+        if (num1 == "" && operator == "") {
             if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*' || element.value == '=' || element.value == '') { //check for negative value before first number in equation
                 if (element.value == '-') {
-                    oldNum = oldNum + element.value;
-                    input.innerHTML = oldNum;
+                    num1 = num1 + element.value;
+                    input.innerHTML = num1;
+                } else if (element.value == '+' || element.value == '/' || element.value == '*') {
+                    operator = element.value;
+                    lengthCheck = 0;
                 }
             } else {
-                oldNum = oldNum + element.value; // only if oldNum == "" and input is not operator
-                input.innerHTML = oldNum;
-                console.log("flag 1")
+                num1 = num1 + element.value;
+                input.innerHTML = num1;
             }
-        } else if (oldNum != "" && currentNum == "") { //evaluates if oldNum is not undefined
-            if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*') {
+        } else if (operator == "") { //only continue if no operator is selected - Keep appending to num1 string
+            if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*' || element.value == '=' || element.value == '') {
                 operator = element.value;
-                input.innerHTML = result;
-                console.log("flag 2")
-            } 
-            else if (operator == "") { //evaluates if oldNum is undefined && operator is undefined
-                oldNum = oldNum + element.value;
-                input.innerHTML = oldNum;
-                console.log("flag 3")
-            } 
-            else if (result == "") { //evaluates if oldNum is undefined && result is undefined
-                currentNum = currentNum + element.value;
-                input.innerHTML = currentNum; //delays result in window until another operator is pressed
-                result = eval(oldNum + operator + currentNum)
-                console.log("flag 4")
-            } 
-            else if (operator != ""){
-                oldNum = result;
-                currentNum = ""; //allowed correct math, but only with single digits
-                currentNum = currentNum + element.value;
-                result = eval(oldNum + operator + currentNum);
-                console.log("flag 5")                          
-                input.innerHTML = currentNum;
-                operator = "";
-            }
-        } else if (currentNum != "") {
-            if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*') {
-                operator = element.value;
-                input.innerHTML = result;
-            } else if (operator == "") { //evaluates if oldNum is undefined && operator is undefined
-                currentNum = currentNum + element.value;
-                input.innerHTML = currentNum;
-                console.log("flag 6")
+                lengthCheck = 0;
             } else {
-                currentNum = currentNum + element.value;
-                input.innerHTML = currentNum; //delays result in window until another operator is pressed
-                result = eval(oldNum + operator + currentNum)
-                console.log("flag 7")
-                oldNum = result;
-                currentNum = "";
-                operator = "";
-            }
-        } else {
-            oldNum = "";
-            currentNum = "";
-            if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*') { 
-                operator = element.value;
-                input.innerHTML = result;
-                console.log("flag 8")
+                num1 = num1 + element.value;
+                input.innerHTML = num1;
+            } //END
+        } 
+        //check if num1, and operator are defined and if so then switch to writing num2
+        else if (num1 && operator) {
+            if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*' || element.value == '=' || element.value == '') { //check for negative value before first number in equation
+                if (element.value == '-') {
+                    num2 = num2 + element.value;
+                    input.innerHTML = num2;
+                    lengthCheck = 1;
+                } else if (element.value == '+' || element.value == '/' || element.value == '*') {
+                    operator = element.value;
+                    lengthCheck = 0;
+                }
             } else {
-                oldNum = result;
-                input.innerHTML = oldNum;
-                operator = "";
-                console.log("flag 9")
+                num2 = num2 + element.value;
+                input.innerHTML = num2;
+                lengthCheck = 1;
+            }
+        } else if (operator == "") { //only continue if no operator is selected - Keep appending to num2 string
+            if (element.value == '+' || element.value == '-' || element.value == '/' || element.value == '*' || element.value == '=' || element.value == '') {
+                operator = element.value;
+                lengthCheck = 0;
+            } 
+        } else if (operator != "") {
+            num2 = num2 + element.value;
+            lengthCheck = 1;
+        }//END
+
+        // num1, operator, num2 input
+        if (num1 && num2 && operator) {
+            if (lengthCheck >= 1) {
+                lengthCheck = 0;
+            } else {
+            result = eval(num1 + operator + num2);
+            input.innerHTML = result;
+            num1 = result;
+            input.innerHTML = num1 + element.value;
+            num2 = "";
+            result = "";
+            operator = "";
             }
         }
+        //num1, operator, num2, then operator clicked (result should appear) should go start back at num1 input
     }
 });
-
-//scenarios
-//1. num1 operator num2, num2, operator
